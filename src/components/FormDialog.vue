83e1c754fcd4672a2a9604c787637b2c328b244c
Kenny Camba Torres <kcambat@outlook.com>
2022-02-18 02:21:23 -0500

    dialog edit, create

---
+++ src/components/FormDialog.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-dialog v-model="dialogModel" max-width="500px">
+    <!-- <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        elevation="0"
+        color="white"
+        class="blue--text text--accent-4"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon>mdi-account-plus</v-icon>
+        <span class="ml-2 hidden-xs-only"></span>
+        <div class="hidden-xs-only">Add User</div>
+      </v-btn>
+    </template> -->
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">{{ title }}</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="editedItem.first_name"
+                label="Name"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="editedItem.last_name"
+                label="Last Name"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="editedItem.email"
+                label="Email"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <!-- <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
+        <v-btn color="blue darken-1" text @click="save"> Save </v-btn> -->
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    dialog: {
+      type: Boolean,
+      default: () => false,
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      editedItem: {
+        first_name: "",
+        last_name: "",
+        email: "",
+      },
+    };
+  },
+  computed: {
+    dialogModel: {
+      get: function (): boolean {
+        return this.dialog;
+      },
+      set: function (value: boolean): void {
+        this.$emit("update:dialog", value);
+      },
+    },
+  },
+});
+</script>
--- src/components/UserTable.vue
+++ src/components/UserTable.vue
@@ -1,19 +1,37 @@
 <template>
   <div>
-    <div class="my-4 d-flex justify-space-between">
-      <div class="heading-6">Users ({{ totalItems }})</div>
-      <v-btn
-        elevation="0"
-        color="white"
-        class="blue--text text--accent-4"
-        v-if="selected.length > 0"
-        @click="openEmail"
-      >
-        <v-icon>mdi-email</v-icon>
-        <span class="ml-2"></span>
-        Send a Message
-      </v-btn>
-    </div>
+    <v-toolbar flat color="transparent" class="my-2" dense>
+      <v-toolbar-title>Users ({{ totalItems }})</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div>
+        <form-dialog
+          :dialog="dialog"
+          :title="dialogTitle"
+          @update:dialog="dialog = $event"
+        />
+        <v-btn
+          elevation="0"
+          color="white"
+          class="blue--text text--accent-4"
+          @click="addUser"
+        >
+          <v-icon>mdi-account-plus</v-icon>
+          <span class="ml-2 hidden-xs-only"></span>
+          <div class="hidden-xs-only">Add User</div>
+        </v-btn>
+        <v-btn
+          elevation="0"
+          color="white"
+          class="blue--text text--accent-4 ml-2"
+          v-if="selected.length > 0"
+          @click="openEmail"
+        >
+          <v-icon>mdi-email</v-icon>
+          <span class="ml-2 hidden-xs-only"></span>
+          <div class="hidden-xs-only">Send a Message</div>
+        </v-btn>
+      </div>
+    </v-toolbar>
     <v-data-table
       :headers="headers"
       :items="items"
@@ -34,6 +52,7 @@
       @item-selected="onItemSelected"
       @toggle-select-all="onToggleSelectAll"
     >
+      <v-toolbar flat></v-toolbar>
       <template v-slot:[`item.avatar`]="{ item }">
         <v-avatar size="36" color="primary">
           <img :src="item.avatar" v-if="item.avatar" />
@@ -46,12 +65,17 @@
           <span> <v-icon small color="blue">mdi-email</v-icon> </span></a
         >
       </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-icon small class="mr-2" @click="editUser(item)"> mdi-pencil </v-icon>
+        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+      </template>
     </v-data-table>
   </div>
 </template>
 <script lang="ts">
 import UserService, { UserDto } from "@/api/userService";
 import Vue from "vue";
+import FormDialog from "./FormDialog.vue";
 
 interface DataType {
   itemsPerPage: number;
@@ -59,11 +83,15 @@ interface DataType {
   pageCount: number;
   totalItems: number;
   loading: boolean;
+  dialog: boolean;
+  dialogTitle: string;
   selected: string[];
   headers: unknown[];
   items: UserDto[];
+  editedItem: UserDto;
 }
 export default Vue.extend({
+  components: { FormDialog },
   data(): DataType {
     return {
       itemsPerPage: 10,
@@ -71,7 +99,16 @@ export default Vue.extend({
       pageCount: 0,
       totalItems: 0,
       loading: true,
+      dialog: false,
+      dialogTitle: "",
       selected: [],
+      editedItem: {
+        id: 0,
+        email: "",
+        first_name: "",
+        last_name: "",
+        avatar: "",
+      },
       headers: [
         {
           text: "Photo",
@@ -92,6 +129,11 @@ export default Vue.extend({
         {
           text: "Email",
           value: "email",
+          class: "subtitle-2",
+        },
+        {
+          text: "Actions",
+          value: "actions",
           class: "subtitle-2",
         },
       ],
@@ -139,6 +181,15 @@ export default Vue.extend({
     },
     openEmail() {
       window.open(`mailto:${this.selected.join(",")}`);
+    },
+    addUser() {
+      this.dialogTitle = "Add User";
+      this.dialog = true;
+    },
+    editUser(item: UserDto) {
+      this.dialogTitle = "Edit User";
+      this.editedItem = item;
+      this.dialog = true;
     },
   },
   mounted() {
